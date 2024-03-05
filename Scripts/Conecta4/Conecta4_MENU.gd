94b8fcd89aedc1dde00a29114bc32285bb62dcd0
Andrés Tejero Mena <150827443+AndresTejeroMena@users.noreply.github.com>
2024-03-05 16:16:01 +0100

    Update

--- Scripts/Conecta4/Conecta4_MENU.gd
+++ Scripts/Conecta4/Conecta4_MENU.gd
@@ -1,21 +1,19 @@
 extends Control
-
 func _ready():
 	pass # Replace with function body.
 
 func _on_normal_pressed():
 	GLOBAL.objetivos_conecta4 = [0,20,19,18,17,16,15,14]
+	GLOBAL.CONECTA_ALEATORIO = false
 func _on_low_pressed():
 	GLOBAL.objetivos_conecta4 = [0,1,2,3,4,5,6,7]
+	GLOBAL.CONECTA_ALEATORIO = false
 func _on_crazy_pressed():
-	var numeros_aleatorios = {}
-	numeros_aleatorios[0] = int(0)
-	numeros_aleatorios = generar_numeros_aleatorios(1, 20, 8)
-	
-	GLOBAL.objetivos_conecta4 = numeros_aleatorios
+	GLOBAL.CONECTA_ALEATORIO = true
 
 func generar_numeros_aleatorios(minimo, maximo, cantidad):
 	var numeros_generados = []
+	numeros_generados.append(0)
 	# Genera números aleatorios sin repeticiones
 	while numeros_generados.size() < cantidad:
 		var numero = randi() % (maximo - minimo + 1) + minimo
@@ -25,6 +23,12 @@ func generar_numeros_aleatorios(minimo, maximo, cantidad):
 
 
 func _on_jugar_pressed():
+	if GLOBAL.CONECTA_ALEATORIO == true:
+		GLOBAL.objetivos_conecta4 = [0,0,0,0,0,0,0,0]
+		var numeros_aleatorios = {}
+		numeros_aleatorios[0] = int(0)
+		numeros_aleatorios = generar_numeros_aleatorios(1, 20, 8)
+		GLOBAL.objetivos_conecta4 = numeros_aleatorios
 	get_tree().change_scene_to_file("res://Escenas/Conecta4_GAME.tscn")
 func _on_atras_pressed():
 	get_tree().change_scene_to_file("res://Escenas/menu.tscn")
